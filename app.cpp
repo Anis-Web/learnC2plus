e414d30a45363e8d4665147a7abcdb7afae2f523
Anis <achadlia0@gmail.com>
2023-05-21 20:11:14 +0300

    adding assign file

--- app.cpp
+++ app.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+// Preprocessor Directive
+/*
+    Syntax for variables
+    - [Data_type] [Variable_Name] = value;
+*/
+/*
+    if we do not want to write "std::" we can write => 
+*/
+using namespace std;
 
 int main()
 {
-    std::cout << "Iam Here\n";
+    int price = 100;
+    std::cout << "Line 1\n";
+    std::cout << "Line 2\n";
+    std::cout << "Line 3\n";
+    // without std::
+    cout << "Price is: 100";
+    cout << "\nPrice after adding 15 is: " << price + 15;
+    cout << "\nPrice after adding 50 is: " << price + 50;
+    //updte the value
+    price = 150;
+    cout << "\nthe new price is: " << price;
+
     return 0;
 }
---
+++ assign.cpp
@@ -0,0 +1,36 @@
+// C++ Assignement
+#include <iostream>
+
+// Assign from 001 to 008
+//Assign 01
+int main() //the main should be a lowercase
+{
+    std::cout << "Line One\n";
+    std::cout << "Line Two\n";
+    std::cout << "Line Three\n";
+    //ass03
+    std::cout << "Iam Learning Programming\nwith\nElzero\nWeb\nSchool\n<3\n";
+    //ass04
+    std::cout << "|=======================|\n|== Elzero Web School ==|\n|==        C++        ==|\n|=======================|\n";
+    return 0;
+}
+//Assign 02
+/*
+    iostream Header File Content
+    - cout
+    - wcout
+    - cin
+    - wcin
+    - cerr
+    - wcerr
+    - clog
+    - wlog
+*/
+//Assign 03
+// int main()
+// {
+//     std::cout << "Iam Learning Programming\nwith\nElzero\nWeb\nSchool\n<3";
+//     return 0;
+// }
+
+// Assign from 009 to 015
