900c5206c3e28c9635d00190421e77b08d30a4e9
Anis <achadlia0@gmail.com>
2023-05-29 21:20:55 +0300

    data types ass

--- assign.cpp
+++ assign.cpp
@@ -1,30 +1,97 @@
 // C++ Assignement
 #include <iostream>
+using namespace std;
 
 // Assign from 001 to 008
 //Assign 01
 int main() //the main should be a lowercase
 {
-    std::cout << "Line One\n";
-    std::cout << "Line Two\n";
-    std::cout << "Line Three\n";
-    //ass03
-    std::cout << "Iam Learning Programming\nwith\nElzero\nWeb\nSchool\n<3\n";
-    //ass04
-    std::cout << "|=======================|\n|== Elzero Web School ==|\n|==        C++        ==|\n|=======================|\n";
-    return 0;
+  std::cout << "Line One\n";
+  std::cout << "Line Two\n";
+  std::cout << "Line Three\n";
+  //ass03
+  std::cout << "Iam Learning Programming\nwith\nElzero\nWeb\nSchool\n<3\n";
+  //ass04
+  std::cout << "|=======================|\n|== Elzero Web School ==|\n|==        C++        ==|\n|=======================|\n";
+
+  cout << "====ass from 9 to 15====\n";
+  cout << "ass 01 =>\n";
+  int age = 40, period = 10;
+  cout << "My Age Is: " << age << "\n";
+  cout << period << " Years Ago My Age Was: " << age - period << "\n";
+  cout << "After" << period << " Years My Age Will Be: " << age + period << "\n";
+  cout << "ass 02 => \n";
+  // Write Your One Line Code Here
+  int a, b, c;
+  // Do Not Edit Below
+  a = 10, b = 20, c = 70;
+  cout << "Sum Of All Number Is: " << a + b + c << "\n";
+  cout << "ass 04 =>  \n";
+  // Write my_text Variable Content Here
+  string my_text = "============\n== Elzero ==\n============\n";
+  // Do Not Edit Below
+  cout << my_text;
+  cout << "ass 05 =>  \n";
+  // Do Not Edit Below
+  int d, e, f = 100;
+  // Add Your Code Here
+  d = e = f;
+  // Do Not Edit Below
+  cout << d + e + f << "\n"; // 300
+  cout << "ass 06 =>  \n";
+  // Do Not Edit Below
+  int j = 210, k = -50, l = -40;
+  // Write Your cout Code Here
+  cout << j - k - l <<  "\n";
+  cout << "ass 08 =>  \n";
+  // Edit Here To Fix The Error
+  int current_year;
+  int birth_year;
+  // Do Not Edit Here
+  current_year = 2022;
+  birth_year = 1982;
+  int age_in_years = current_year - birth_year;
+  cout << age_in_years << "\n";
+  // cout << "ass 09 =>  \n";
+  // Write Your Code Here
+  // int number;
+  // cin >> number;
+  // cout << number * number << "\n";
+  // Num 10
+  // Needed Output 100 [10 * 10]
+  // cout << "ass 10 =>  \n";
+  // Write Your Code Here
+  // int numOne, numTwo;
+  // cin >> numOne;
+  // cin >> numTwo;
+  // cout << numOne * numTwo << "\n";
+  // Number One => 10
+  // Number Two => 100
+  // Needed Output 1000 [10 * 100]
+  cout << "ass 11 =>  \n";
+  cout << "Bello\nHello Elzeroo";
+  cout << " Web Schoolll";
+  cout << " I Love Programming";
+  cout << " Too Much";
+  cout << " Specially C++\n";
+  // Needed Output
+  // "Hello Elzero Web School I Love Programming Too Much Specially C++"
+  cout << "=========================\n";
+  cout << "====ass from 16 to 23====\n";
+  cout << "=========================\n";
+  return 0;
 }
 //Assign 02
 /*
-    iostream Header File Content
-    - cout
-    - wcout
-    - cin
-    - wcin
-    - cerr
-    - wcerr
-    - clog
-    - wlog
+  iostream Header File Content
+  - cout
+  - wcout
+  - cin
+  - wcin
+  - cerr
+  - wcerr
+  - clog
+  - wlog
 */
 //Assign 03
 // int main()
