4fe070df1fd75bfccc8a56bca4449e980047a3f6
Anis <achadlia0@gmail.com>
2023-06-04 19:13:33 +0300

    ass from 16 to 23

--- assign.cpp
+++ assign.cpp
@@ -1,5 +1,6 @@
 // C++ Assignement
 #include <iostream>
+#include <climits>
 using namespace std;
 
 // Assign from 001 to 008
@@ -79,6 +80,92 @@ int main() //the main should be a lowercase
   cout << "=========================\n";
   cout << "====ass from 16 to 23====\n";
   cout << "=========================\n";
+  cout << "ass 02 =>  \n";
+  double salary = 5000.98;
+  cout << sizeof(salary) << " Bytes\n";
+  cout << sizeof(salary)*CHAR_BIT << " Bits\n";
+  cout << "ass 03 =>  \n";
+  cout << "Maximum integer Number is => " << INT_MAX << "\n";
+  cout << "Minimum integer Number is => " << INT_MIN << "\n";
+  cout << "Maximum short integer Number is => " << SHRT_MAX << "\n";
+  cout << "Minimum short integer Number is => " << SHRT_MIN << "\n";
+  cout << "ass 04 =>  \n";
+  // Edit Line To Make 8 Become 4
+  cout << sizeof(10.5f + 5 + 20.5f + 10) << "\n"; // 4
+  cout << "ass 05 =>  \n";
+  char char_a = '~',char_b = '&',char_c = '%',char_d = 'A';
+  cout << "ASCII Value of " << char_a << " is: " << int(char_a) << "\n";
+  cout << "ASCII Value of " << char_b << " is: " << int(char_b) << "\n";
+  cout << "ASCII Value of " << char_c << " is: " << int(char_c) << "\n";
+  cout << "ASCII Value of " << char_d << " is: " << int(char_d) << "\n";
+  cout << "ass 06 =>  \n";
+  int int_a = 69, int_b = 108;
+  cout << "Character of this ASCII value " << int_a << "is: " << char(int_a) << "\n";
+  cout << "Character of this ASCII value " << int_b << "is: " << char(int_b) << "\n";
+  cout << "ass 07 =>  \n";
+  // cout << char(1) << "\n"; // SOH (start of heading)
+  // cout << char(13) << "\n"; // Carriage Return or CR
+  // cout << char(17) << "\n"; // Device Control 1
+  // cout << char(70) << "\n"; // F
+  // we have to find EWS 
+  // => E = 69, W = 87, S = 83
+  int n_one = 1, n_two = 13, n_three = 17, n_foor = 70;
+  cout << char(n_foor - n_one) << char(n_foor + n_three) << char(n_foor + n_two) << "\n"; // EWS
+  cout << "ass 08 =>  \n";
+  short int a_in_bs = 100;
+  long int b_in_bs = 15001500;
+  long double c_in_bs = 100.54565746;
+
+  // Do Not Edit
+  cout << sizeof(a_in_bs) << " Bytes\n"; // 2 Bytes
+  cout << sizeof(b_in_bs) << " Bytes\n"; // 8 Bytes
+  cout << sizeof(c_in_bs) << " Bytes\n"; // 16 Bytes
+  cout << "ass 10 =>  \n";
+  // Add Type Alias Here
+  // using lli = long long int;
+  typedef long long int lli;
+  // Do Not Edit
+  lli num = 150050005; // This Is Long Long Int
+  cout << num << "\n"; // 150050005
+  cout << "ass 11 =>  \n";
+  // Do Not Edit Here
+  short int x = 100;
+  float y = 98.5f;
+  double z = 1.7;
+
+  // Edit This Line And Use Type Casting To Get The Output
+  cout << int(x + y + z) << "\n"; // 200
+  cout << "ass 12 =>  \n";
+  // Do Not Edit Here
+  short a12 = 1000;
+  int b12 = 10000;
+  long double c12 = 5.560000505012;
+
+  // Change ??? To Something Else To Get The Output
+  cout << sizeof(c12) - sizeof(b12)<< "\n"; // 12
+  cout << sizeof(c12) + sizeof(b12) << "\n"; // 20
+  cout << sizeof(c12) * sizeof(a12) << "\n"; // 32
+  cout << a12 * int(c12) << "\n"; // 5000
+  cout << char(sizeof(c12) * int(c12)) << "\n"; // P
+  cout << "=========================\n";
+  cout << "====ass from 24 to 29====\n";
+  cout << "=========================\n";
+  cout << "ass 01 =>  \n";
+
+  cout << "ass 02 =>  \n";
+
+  cout << "ass 03 =>  \n";
+
+  cout << "ass 04 =>  \n";
+
+  cout << "ass 05 =>  \n";
+
+  cout << "ass 06 =>  \n";
+
+  cout << "ass 07 =>  \n";
+  
+  cout << "ass 08 =>  \n";
+  
   return 0;
 }
 //Assign 02
